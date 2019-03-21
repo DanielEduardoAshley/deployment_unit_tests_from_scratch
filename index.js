a97005125212f30ea78d0c0a2d50b2e996ac3e16
daniel ashley <danielashley@pursuit.org>
2019-03-20 20:24:09 -0400

    added gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+const express= require('express')
+const app = express()
+
+app.get('/ping', (req,res)=>{
+
+    res.status(200)
+    res.json({'ping' : " 🍑 "})
+
+})
+
+const port = 5001
+app.listen(port, ()=>{
+
+    console.log(`listening on port ${port}`)
+})
